6855fce29e9854b33dd240ba20427fc649b2c037
Oscar <oscar.ramos@ucsp.edu.pe>
2020-05-15 00:59:25 -0500

    Updates

---
+++ precache-manifest.dfbf7e0e71676a1f4c06bd199d34a744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad84749813635a57590a6e371b97982",
+    "url": "/word-frequency/index.html"
+  },
+  {
+    "revision": "2a84117040a8914205ab",
+    "url": "/word-frequency/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b28538669b28855b231",
+    "url": "/word-frequency/static/js/2.9f5c5159.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/word-frequency/static/js/2.9f5c5159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a84117040a8914205ab",
+    "url": "/word-frequency/static/js/main.239bb63a.chunk.js"
+  },
+  {
+    "revision": "f1e111a20ac9a8789ea7",
+    "url": "/word-frequency/static/js/runtime-main.e990549d.js"
+  }
+]);
